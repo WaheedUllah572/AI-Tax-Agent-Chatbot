b080629fd893bcd61e2ba1fb78a5c3f341370a93
Waheed Ullah <wu0907020@gmail.com>
2025-07-30 20:42:31 +0500

    Cleaned up for proper Streamlit deployment

--- api/index.py
+++
@@ -1,6 +0,0 @@
-from streamlit.web import cli as stcli
-import sys
-
-if __name__ == "__main__":
-    sys.argv = ["streamlit", "run", "app.py", "--server.enableCORS=false"]
-    sys.exit(stcli.main())
